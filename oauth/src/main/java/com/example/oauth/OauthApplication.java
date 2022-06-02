1e34d6ccc26da9257814efc30c6bed8f27a2b7b8
John <johncomstock9716@gmail.com>
2022-06-02 09:29:07 -0400

    Initial commit

---
+++ oauth/src/main/java/com/example/oauth/OauthApplication.java
@@ -0,0 +1,75 @@
+package com.example.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+@SpringBootApplication
+@RestController
+public class OauthApplication extends WebSecurityConfigurerAdapter {
+
+
+	@GetMapping("/user")
+	public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+		return Collections.singletonMap("name", principal.getAttribute("name"));
+	}
+
+	@GetMapping("/error")
+	public String error(HttpServletRequest request) {
+		String message = (String) request.getSession().getAttribute("error.message");
+		request.getSession().removeAttribute("error.message");
+		return message;
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthApplication.class, args);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// @formatter:off
+		http
+				.authorizeRequests(a -> a
+						.antMatchers("/", "/error", "/webjars/**").permitAll()
+						.anyRequest().authenticated()
+
+				)
+				.logout(l -> l
+						.logoutSuccessUrl("/").permitAll()
+				)
+				.csrf(c -> c
+						.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+				)
+				.exceptionHandling(e -> e
+						.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+				.oauth2Login(o -> o
+						.failureHandler((request, response, exception) -> {
+							request.getSession().setAttribute("error.message", exception.getMessage());
+						}
+				));
+	}
+				// @formatter:on
+	}
+
